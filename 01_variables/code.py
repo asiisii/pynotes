836721c83991433d257b61ee91d43ea995afe7d0
asiisii <ashishmalla45@gmail.com>
2023-07-04 21:32:36 -0600

    add notes on variables

---
+++ 01_variables/code.py
@@ -0,0 +1,37 @@
+# -- Numbers and Floats --
+# Integers are whole numbers.
+# Floats are nubmers with a floating point (or decimal place)
+x = 5 
+price = 9.99
+
+discount = 0.8
+
+result = price * (1 - discount)
+
+print(result)
+
+# -- Strings --
+# strings are used to store characters
+name = "Aziz"
+name = "Habibi"
+
+print(name)
+print(name * 2) #output: HabibiHabibi
+
+# -- Changing variables --
+# Variables are names for values.
+
+a = 25
+b = a
+
+# Here we've given the value '25' the names 'a' and 'b'.
+
+print(a)
+print(b)
+
+b = 17
+
+# Here we've given the value '17' the name 'b'. The name 'a' is still a name for '25'!
+
+print(a) #output: 25
+print(b) #output: 17
